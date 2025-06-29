ce96f315a1b2bb729ccca8f309720b4aacfd162f
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-06-29 11:17:51 +0500

    add and sub

---
+++ day1_linear algebra.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+# Matrix addition
+C = A + B
+# Matrix subtraction
+D = A - B
+print("Matrix addition:\n", C)
+print("Matrix subtraction:\n", D)
