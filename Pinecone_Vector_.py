683c4638be8316df5b4f7033f80beec93f1c17d1
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 15:22:37 +0500

    pinecone

--- Pinecone_Vector_.py
+++ Pinecone_Vector_.py
@@ -5,5 +5,20 @@ import os
 # Load environment variables
 load_dotenv()
 # Initialize Pinecone
-pinecone = Pinecone(
+pc = Pinecone(
     api_key=os.getenv("PINECONE_API_KEY"))
+# Create your Pinecone index
+# Create your Pinecone index
+pc.create_index(
+    name="my-first-index",
+    dimension=256,
+    spec=ServerlessSpec(
+        cloud='aws',
+        region='us-east-1'
+    )
+)
+# describing index stats
+index = pc.Index("my-first-index")
+index.describe_index_stats()
+pc.delete_index("my-first-index")
+pc.list_indexes()
