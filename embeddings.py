7ecb4ceeba1f6e64ebedb49ae449ffe7816a852b
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 10:22:15 +0500

    tsne

--- embeddings.py
+++ embeddings.py
@@ -12,3 +12,14 @@ response_Dict = response.model_dump()
 for i, product in enumerate(products):
     products['embedding'] = response_Dict['data'][i]['embedding']
 
+# working is t-sne
+from scikit.manifold import TSNE
+import pandas as pd
+import numpy as np
+
+embeddings = np.array([product['embedding'] for product in products])
+categories = [product['category'] for product in products]
+tsne = TSNE(n_components=2, perplexity=4)
+embeddings_2d = tsne.fit_transform(embeddings)
+matplotlib.pyplot.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=categories, cmap='viridis')
+pyplot.show()
