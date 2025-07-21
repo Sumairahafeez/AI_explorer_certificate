399abd07652653d7514e9e75d8139a2532aae25e
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 11:20:26 +0500

    ..

--- embeddings.py
+++ embeddings.py
@@ -113,3 +113,18 @@ hits = find_n_closest(mean_history_embeddings, product_embeddings)
 for hit in hits:
   product = products_filtered[hit['index']]
   print(product['title']) 
+
+
+# Define a function to return the minimum distance and its index
+def find_closest(query_vector, embeddings):
+  distances = []
+  for index, embedding in enumerate(embeddings):
+    dist = distance.cosine(query_vector, embedding)
+    distances.append({"distance": dist, "index": index})
+  return min(distances, key=lambda x: x["distance"])
+
+for index, review in enumerate(reviews):
+  # Find the closest distance and its index using find_closest()
+  closest =find_closest(review_embeddings[index], class_embeddings)
+  label = sentiments[closest['index']]['label']
+  print(f'"{review}" was classified as {label}')
