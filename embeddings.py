f48e0ec3c25e265ed49e2d21af5a71d9facd646c
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 11:06:12 +0500

    articles

--- embeddings.py
+++ embeddings.py
@@ -94,3 +94,22 @@ hits = find_n_closest(last_product_embeddings, product_embeddings)
 for hit in hits:
   product = products[hit['index']]
   print(product['title'])
+
+
+ # Prepare and embed the user_history, and calculate the mean embeddings
+history_texts = [create_product_text(article) for article in user_history]
+history_embeddings = create_embeddings(history_texts)[0]
+mean_history_embeddings = np.mean(history_embeddings, axis=0)
+
+# Filter products to remove any in user_history
+products_filtered = [product for product in products if product not in user_history]
+
+# Combine product features and embed the resulting texts
+product_texts = [create_product_text(product) for product in products_filtered]
+product_embeddings = create_embeddings(product_texts)[0]
+
+hits = find_n_closest(mean_history_embeddings, product_embeddings)
+
+for hit in hits:
+  product = products_filtered[hit['index']]
+  print(product['title']) 
