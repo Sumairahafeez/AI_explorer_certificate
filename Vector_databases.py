1607dcf40e8bcf424d9b3be8fd76eb71ac4f226d
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 12:10:26 +0500

    chromadb

---
+++ Vector_databases.py
@@ -0,0 +1,36 @@
+# chroma DB is used for vector databases for storing and querying embeddings
+import chromadb
+
+# Retrieve the netflix_titles collection
+collection = client.get_collection(
+  name="netflix_titles",
+  embedding_function=OpenAIEmbeddingFunction(model_name="text-embedding-3-small", api_key="<OPENAI_API_TOKEN>")
+)
+
+# Query the collection for "films about dogs"
+result = collection.query(
+  query_texts = ["films about dogs"],
+  n_results = 3)
+
+print(result)
+
+collection = client.get_collection(
+  name="netflix_titles",
+  embedding_function=OpenAIEmbeddingFunction(model_name="text-embedding-3-small", api_key="<OPENAI_API_TOKEN>")
+)
+
+# Update or add the new documents
+collection.add(
+    ids= [doc['id'] for doc in new_data],
+    documents=[doc['document'] for doc in new_data]
+)
+
+# Delete the item with ID "s95"
+collection.delete(
+  ids = 's95'
+)
+result = collection.query(
+    query_texts=["films about dogs"],
+    n_results=3
+)
+print(result)
