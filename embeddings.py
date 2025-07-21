6f10284a8b57bfc7c67662ffa9f17911f9a66b19
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-21 10:33:19 +0500

    ..

--- embeddings.py
+++ embeddings.py
@@ -23,3 +23,22 @@ tsne = TSNE(n_components=2, perplexity=4)
 embeddings_2d = tsne.fit_transform(embeddings)
 matplotlib.pyplot.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1], c=categories, cmap='viridis')
 pyplot.show()
+# measuring similarity between vectors using cosine function
+# creating a function to calculate cosine similarity
+
+def create_embeddings(text):
+    client = OpenAIEmbeddings()
+    response = client.embeding.create(
+        model='text-embedding-3-small',
+        input=text
+    )
+    return response.model_dump()['data'][0]['embedding']
+
+search_text = "soap"
+search_embedding = create_embeddings(search_text)
+distance = []
+for product in products:
+    product['similarity'] = cosine_similarity(search_embedding, product['embedding'])
+    distance.append(product['similarity'])
+min_distance = min(distance)
+most_similar_product = products[distance.index(min_distance)]
