73f090052b99e587d1f77131275b7c2553960bf3
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-22 14:17:05 +0500

    batches

--- Pinecone_Vector_.py
+++ Pinecone_Vector_.py
@@ -86,4 +86,14 @@ for chunk in chunks(vectors):
 
 
 # Retrieve statistics of the connected Pinecone index
-print(index.describe_index_stats())        
+print(index.describe_index_stats())  
+
+index = pc.Index('datacamp-index')
+
+# Upsert vectors in batches of 200 vectors
+with pc.Index('datacamp-index', pool_threads = 20) as index:
+    async_results = [index.upsert(vectors=chunk, async_req=True) for chunk in chunks(vectors, batch_size=200)]
+    [async_result.get() for async_result in async_results]
+
+# Retrieve statistics of the connected Pinecone index
+print(index.describe_index_stats())
