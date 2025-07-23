1dd7b47c4cb62e128ff43372e31f4c41dda110d6
Sumairahafeez <sumairahafeezfp@gmail.com>
2025-07-23 13:10:03 +0500

    Rag splitting

--- LLM.py
+++ LLM.py
@@ -21,3 +21,90 @@ tools = load_tools(["llm-math"], llm=llm)
 agent = create_react_agent(llm, tools)
 messages = agent.invoke({"messages": [("human", "What is the square root of 101?")]})
 print(messages)
+
+# Convert the retrieve_customer_info function into a tool
+@tool
+def retrieve_customer_info(name: str) -> str:
+    """Retrieve customer information based on their name."""
+    customer_info = customers[customers['name'] == name]
+    return customer_info.to_string()
+  
+# Print the tool's arguments
+print(retrieve_customer_info.args)
+# Create a ReAct agent
+agent = create_react_agent(llm, [retrieve_customer_info])
+
+# Invoke the agent on the input
+messages = agent.invoke({"messages": [("human", "Create a summary of our customer: Peak Performance Co.")]})
+print(messages['messages'][-1].content)
+
+# Import library
+from langchain_community.document_loaders import PyPDFLoader
+
+# Create a document loader for rag_vs_fine_tuning.pdf
+loader = PyPDFLoader('rag_vs_fine_tuning.pdf')
+
+# Load the document
+data = loader.load()
+print(data[0])
+
+# Import library
+from langchain_community.document_loaders.csv_loader import CSVLoader
+
+# Create a document loader for fifa_countries_audience.csv
+loader = CSVLoader('fifa_countries_audience.csv')
+
+# Load the document
+data = loader.load()
+print(data[0])
+
+
+# Create a document loader for unstructured HTML
+loader = UnstructuredHTMLLoader('white_house_executive_order_nov_2023.html')
+
+# Load the document
+data = loader.load()
+
+# Print the first document
+print(data[0])
+
+# Print the first document's metadata
+print(data[0].metadata)
+
+# Import the character splitter
+from langchain_text_splitters import CharacterTextSplitter
+
+quote = 'Words are flowing out like endless rain into a paper cup,\nthey slither while they pass,\nthey slip away across the universe.'
+chunk_size = 24
+chunk_overlap = 10
+
+# Create an instance of the splitter class
+splitter = CharacterTextSplitter(
+    separator = '\n',
+    chunk_size = chunk_size,
+    chunk_overlap = chunk_overlap
+)
+
+# Split the string and print the chunks
+docs = splitter.split_text(quote)
+print(docs)
+print([len(doc) for doc in docs])
+
+# Import the recursive character splitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+quote = 'Words are flowing out like endless rain into a paper cup,\nthey slither while they pass,\nthey slip away across the universe.'
+chunk_size = 24
+chunk_overlap = 10
+
+# Create an instance of the splitter class
+splitter = RecursiveCharacterTextSplitter(
+    separators = ['\n',' ',''],
+    chunk_size=chunk_size,
+    chunk_overlap = chunk_overlap
+)
+
+# Split the document and print the chunks
+docs = splitter.split_text(quote)
+print(docs)
+print([len(doc) for doc in docs])
